7bd592521bfc29e0ef030493632967b2f3ae76b4
ziv221 <ziv.itzh@gmail.com>
2020-09-05 10:43:42 +0300

    fixed url encoding problem

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,12 @@ const router    = express.Router()
 const urllib    = require("urllib")    
 
 router.get("/movies/:movie", function(req,res){
-    const movieName = req.params.movie
-    const url = `http://www.omdbapi.com/?s=${movieName}}&apikey=14b0dd81`
+    const movieName = encodeURIComponent(req.params.movie)
+    const url = `http://www.omdbapi.com/?s=${movieName}}&plot=full&apikey=14b0dd81`
 
     urllib.request(url, function (err, data) {       
-        res.send(JSON.parse(data).Search)
+        const modifiedData = JSON.parse(data).Search
+        res.send(modifiedData)
     });
         
 })
